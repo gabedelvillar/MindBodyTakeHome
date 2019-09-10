5b4abc3ba7b6d07aa58f8d5b32b2fe9722172771
Gabriel Del Villar <gabedelvillar@gmail.com>
2019-09-10 13:19:43 -0700

    add collection view to country details view controller

--- MindBodyHW/MindBodyHW/Controller/CountryDetailsViewController.swift
+++ MindBodyHW/MindBodyHW/Controller/CountryDetailsViewController.swift
@@ -8,9 +8,45 @@
 
 import UIKit
 
-class CountryDetailsViewController: UIViewController {
+class CountryDetailsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  fileprivate let cellId = "CellId"
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    cell.backgroundColor = .blue
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return .init(width: view.frame.width, height: 100)
+  }
+  
+  
+  lazy var collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    cv.delegate = self
+    cv.dataSource = self
+    return cv
+  }()
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .red
+    
+    setupCollectionView()
+    
+    
+  }
+  
+  fileprivate func setupCollectionView() {
+    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    collectionView.backgroundColor = .white
+    view.addSubview(collectionView)
+    collectionView.fillSuperview()
   }
 }
